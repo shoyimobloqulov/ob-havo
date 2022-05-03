a4d1258aee1d4a2251b2705ba47e72a789ae82b3
Shoyim Obloqulov <92427513+ShoyimObloqulov@users.noreply.github.com>
2022-05-03 13:26:34 -0700

    Delete check.py

--- check.py
+++
@@ -1,6 +0,0 @@
-import requests
-import os
-
-app_id = os.getenv('api')
-r=requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat=39.607773&lon=66.977065&appid={api_id}").json()
-print(r["coord"]["lat"])
